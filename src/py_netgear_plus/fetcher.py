259d0ee1d2d8c308e65babbc255b368af5d1e7c6
Michiel Fokke <github@fokke.org>
2026-04-10 08:15:47 +0100

    feat: add MS108EUP support (rebased onto main)
    Co-authored-by: Ada <ada@nanoclaw.ai>

--- src/py_netgear_plus/fetcher.py
+++ src/py_netgear_plus/fetcher.py
@@ -35,6 +35,11 @@ class LoginFailedError(Exception):
     """Invalid credentials."""
 
 
+class MaxSessionsError(Exception):
+    """Maximum number of sessions reached on the switch."""
+
+
+
 class NotLoggedInError(Exception):
     """Not logged in."""
 
